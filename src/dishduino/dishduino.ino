fa98d5e5555602269572be13efd5dff6fe790b50
Jason Dufair <jase@dufair.org>
2020-01-05 15:57:12 -0500

    Initial commit

---
+++ src/dishduino/dishduino.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
+#include "Ringbuf.h"
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+
+// 30 sec of z axis samples
+#define RING_BUFFER_SIZE 30
+RingBuf<float, RING_BUFFER_SIZE> ringBuffer;
+
+void setup() {
+  Serial.begin(9600);
+  initializeMma();
+  ringBuffer.clear();
+}
+
+void loop() {
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  mma.getEvent(&event);
+  sampleData(event);
+  
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(event.acceleration.x);
+  Serial.print(" Y: "); Serial.print(event.acceleration.y);
+  Serial.print(" Z: "); Serial.println(event.acceleration.z);
+  Serial.println();
+  delay(1000);
+}
+
+void initializeMma() {
+  if (! mma.begin()) {
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  Serial.println("MMA8451 found!");
+  mma.setRange(MMA8451_RANGE_2_G);
+  Serial.print("Range = "); Serial.print(2 << mma.getRange());  
+  Serial.println("G");  
+}
+
+void sampleData(sensors_event_t event) {
+  float data;
+  if (ringBuffer.isFull())
+    ringBuffer.pop(data);
+  ringBuffer.push(event.acceleration.z);
+  printRingBuffer();  
+}
+
+void printRingBuffer() {
+  const int size = ringBuffer.size();
+  for (int i = 0; i < size; i++) {
+    Serial.print(ringBuffer[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
